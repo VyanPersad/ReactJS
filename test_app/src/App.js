c918b02b081ff089c97e3cebefb55a8be0b57b82
VyanPersad <vyanpersad@gmail.com>
2025-05-24 03:20:05 -0400

    Some more experimentation.

--- test_app/src/App.js
+++ test_app/src/App.js
@@ -1,10 +1,13 @@
-
 import './App.css';
+import { Header } from './components/Header';
+import { Footer } from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      
+      <Header/>
+      <div>This is where the Body goes</div>
+      <Footer/>
     </div>
   );
 }
